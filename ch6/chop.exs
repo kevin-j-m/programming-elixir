a61c416ec6601edfebe2cf9c94d66f7b32325426
Kevin Murphy <(none)>
2016-06-05 13:30:32 -0400

    Guess a number within a range

---
+++ ch6/chop.exs
@@ -0,0 +1,22 @@
+defmodule Chop do
+  def guess(n, a..b) do
+    diff = b - a
+    guess = div(diff, 2) + a
+
+    IO.puts "Is it #{guess}"
+
+    check(n, guess, a..b)
+  end
+
+  def check(actual, guess, _low.._high)
+    when actual == guess,
+    do: IO.puts "#{actual}"
+
+  def check(actual, guess, _low..high)
+    when actual > guess,
+    do: guess(actual, (guess + 1)..high)
+
+  def check(actual, guess, low.._high)
+    when actual < guess,
+    do: guess(actual, low..(guess - 1))
+end
