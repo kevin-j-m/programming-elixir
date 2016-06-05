d8b08be7320cb413005495807e42da38e794ce12
Kevin Murphy <murphy.kevin.mail@gmail.com>
2016-06-05 16:39:40 -0400

    Initial max implementation
    This does not appropriately handle negative numbers, but was my first
    implementation for writing a max.

--- ch7/my_list.exs
+++ ch7/my_list.exs
@@ -8,4 +8,12 @@ defmodule MyList do
   defp _mapsum([head | tail], func, sum) do
     _mapsum(tail, func, sum + func.(head))
   end
+
+  def max([head | tail]), do: _max([head | tail], 0)
+
+  defp _max([], max), do: max
+  defp _max([head | tail], current_max) when current_max > head do
+    _max(tail, current_max)
+  end
+  defp _max([head | tail], _current_max), do: _max(tail, head)
 end
