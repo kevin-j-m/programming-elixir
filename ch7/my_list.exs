787ae7c9c98d02af359d81e5c9402d164b81cf52
Kevin Murphy <murphy.kevin.mail@gmail.com>
2016-06-05 16:25:27 -0400

    mapsum function
    Applies the function to each element in the list and returns the sum of
    the result.

--- ch7/my_list.exs
+++ ch7/my_list.exs
@@ -1,4 +1,11 @@
 defmodule MyList do
   def sum([]), do: 0
   def sum([head | tail]), do: head + sum(tail)
+
+  def mapsum([head | tail], func), do: _mapsum([head | tail], func, 0)
+
+  defp _mapsum([], _func, sum), do: sum
+  defp _mapsum([head | tail], func, sum) do
+    _mapsum(tail, func, sum + func.(head))
+  end
 end
